6a1bf8aca98e583cf85c7cac569a2fe5c9126418
Ashish Shevale <shevaleashish@gmail.com>
2021-12-27 23:29:50 +0530

    Created basic methods for workspace manipulation

---
+++ server/models/hierarchy/workspaceModel.ts
@@ -0,0 +1,34 @@
+import { CRUD_CreateFunc, CRUD_DeleteFunc, CRUD_ReadFunc, CRUD_UpdateFunc } from "./types";
+
+type WorkspaceDataType = {
+  id: string,
+  name: string,
+}
+
+const createWorkspaceForUser: CRUD_CreateFunc<WorkspaceDataType> = (data: WorkspaceDataType, parentId: string): boolean => {
+  return false;
+}
+
+const getWorkspaceFromId: CRUD_ReadFunc<WorkspaceDataType> = (itemId: string): WorkspaceDataType => {
+  const item: WorkspaceDataType = {
+    id: "",
+    name: "",
+  };
+
+  return item;
+}
+
+const updateWorkspaceData: CRUD_UpdateFunc<WorkspaceDataType> = (itemId: string, newData: WorkspaceDataType): boolean => {
+  return false;
+}
+
+const deleteWorkspaceFromId: CRUD_DeleteFunc<WorkspaceDataType> = (itemId: string): boolean => {
+  return false;
+}
+
+export {
+  createWorkspaceForUser,
+  getWorkspaceFromId,
+  updateWorkspaceData,
+  deleteWorkspaceFromId,
+};
