e3bd7cbef10c0cdc78a98d28a31f602b81ece5ab
Ashish Shevale <shevaleashish@gmail.com>
2021-12-28 01:05:58 +0530

    Create basic controller for auth

---
+++ server/controllers/authController.ts
@@ -0,0 +1,33 @@
+import { RequestHandler } from "express"
+
+const registerUser: RequestHandler = (req, res, next) => {
+  const { userId, userEmail, password } = req.body;
+  console.log(userId, userEmail, password);
+
+  res.end("Register User ...");
+}
+
+const loginUser: RequestHandler = (req, res, next) => {
+  const { userEmail, password } = req.body;
+  console.log(userEmail, password);
+
+  res.end("Login User ...");
+}
+
+const forgotPassword: RequestHandler = (req, res, next) => {
+  const { userEmail } = req.body;
+  console.log(userEmail);
+
+  res.end("Forgot Password ...");
+}
+
+const resetPassword: RequestHandler = (req, res, next) => {
+  res.end("Reset Password ...");
+}
+
+export {
+  registerUser,
+  loginUser,
+  forgotPassword,
+  resetPassword,
+}
