bf5352c72c88786b0aa1137186fc558c53de9591
Ashish Shevale <shevaleashish@gmail.com>
2021-12-27 23:19:16 +0530

    Created basic setup for user related functions.

---
+++ server/models/userModel.ts
@@ -0,0 +1,31 @@
+type UserDataType = {
+  userId: string,
+  userEmail: string,
+  userPassword?: string,
+};
+
+function createUserWithPassword(userData: UserDataType): boolean {
+  return false;
+}
+
+function getUserFromEmail(userEmail: string): UserDataType {
+  const userData: UserDataType = {
+    userId: "",
+    userEmail: "",
+    userPassword: "",
+  };
+
+  return userData;
+}
+
+function updatePasswordForUser(userData: UserDataType, newPassword: string): boolean {
+  return false;
+}
+
+export {
+  UserDataType,
+
+  createUserWithPassword,
+  getUserFromEmail,
+  updatePasswordForUser,
+};
