6afdb596194dc206a2c9cc3b6e4f2637b41a6239
Ashish Shevale <shevaleashish@gmail.com>
2021-12-31 12:04:20 +0530

    Install packages

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,18 @@
-import express from "express";
-const app = express();
+require("dotenv").config({ path: "./config.env" });
 
-app.listen(3000, () => {
-  console.log("STARTED\n");
+import express from "express";
+import authRouter from "./routes/auth";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Global Middlewares.
+app.use(express.json());
+
+// Routes.
+app.use("/api/auth", authRouter);
+
+// Start Server.
+app.listen(PORT, () => {
+  console.log(`> Server started on port ${PORT}`);
 });
