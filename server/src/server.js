7887a700ce0e6dac43e68caab42652b3ae6b4935
Ashish Shevale <shevaleashish@gmail.com>
2022-01-07 23:43:05 +0530

    Allow cross origin requests

--- server/src/server.js
+++ server/src/server.js
@@ -10,6 +10,15 @@ const errorHandler = require("./middleware/error");
 const notFound = require("./middleware/notFound");
 const connectDB = require("./config/db");
 
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(require('cors')());
+
 app.use(express.json())
 
 // Routes
