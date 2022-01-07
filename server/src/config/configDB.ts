53edd172448948e2734d385d48ad9809763f194b
Ashish Shevale <shevaleashish@gmail.com>
2022-01-07 20:09:48 +0530

    Create config file connect to database

--- server/src/config/configDB.ts
+++ server/src/config/configDB.ts
@@ -1,1 +1,10 @@
-export { }
+import mongoose from "mongoose";
+
+const connectDB = async (url: string) => {
+  await mongoose.connect(url), {
+    userNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+}
+
+export default connectDB;
