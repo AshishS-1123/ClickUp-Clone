08d487907b6ff042a61d9d521a93cbeb2061dac0
Ashish Shevale <shevaleashish@gmail.com>
2022-01-08 12:28:21 +0530

    Plugin for importing google fonts

---
+++ client/next.config.js
@@ -0,0 +1,9 @@
+const { withGoogleFonts } = require("nextjs-google-fonts");
+
+module.exports = withGoogleFonts({
+  googleFonts: {
+    fonts: [
+      "https://fonts.googleapis.com/css2?family=Roboto:wght@400;900&display=swap",
+    ],
+  },
+});
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,20 @@
+import Head from "next/head";
+import React from "react";
+import { GoogleFonts } from "nextjs-google-fonts/GoogleFonts";
 import '../styles/global_styles.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;900&display=swap" rel="stylesheet" />
+        {
+          GoogleFonts()
+        }
+      </Head>
+      <Component {...pageProps} />
+    </React.Fragment>
+  )
 }
