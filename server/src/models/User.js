14858472decd18d4f98cdc667e0948f7bc17d23a
Ashish Shevale <shevaleashish@gmail.com>
2022-01-10 15:05:23 +0530

    Use sparse indices

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -9,6 +9,7 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please provide a email."],
     unique: true,
+    sparse: true,
     match: [
       /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
       "Please provide a valid email."
