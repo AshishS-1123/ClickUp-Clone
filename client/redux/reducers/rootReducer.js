fb87c32d68399a09d1f6b48e29e75994863870df
Ashish Shevale <shevaleashish@gmail.com>
2022-01-10 15:08:26 +0530

    Root reducer

--- client/redux/reducers/rootReducer.js
+++ client/redux/reducers/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 
+console.log(combineReducers);
+
 export default rootReducer = combineReducers({
   auth: authReducer,
 });
