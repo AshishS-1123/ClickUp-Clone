acb601800bd89cbb1cca8207612d920f82368786
Ashish Shevale <shevaleashish@gmail.com>
2022-01-12 19:54:56 +0530

    Navbar component

---
+++ client/features/NavBar/NavDrawer/index.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import DoubleArrowOutlinedIcon from '@mui/icons-material/DoubleArrowOutlined';
+import styles from "./NavDrawer.module.css";
+
+const DrawerToggler = ({ onClick, rotateBtn }) => {
+  const styles = {
+    transform: rotateBtn == true ? "rotate(180deg)" : "rotate(0deg)",
+    fill: "#ffa12f",
+  }
+  return (
+    <IconButton aria-label="toggle-navbar" onClick={onClick}>
+      <DoubleArrowOutlinedIcon style={styles} />
+    </IconButton>
+  )
+}
+
+function NavDrawer() {
+  const [drawerState, setDrawerState] = useState(true);
+  const toggleDrawer = () => {
+    setDrawerState(state => !state);
+  };
+
+  return (
+    <>
+      <DrawerToggler onClick={toggleDrawer} />
+      <Drawer
+        className={styles.drawer}
+        anchor="left"
+        open={drawerState}
+        variant="persistent"
+      >
+        <Button
+          className={styles.buttonText}
+          variant="text"
+          disableRipple={true}
+          endIcon={<DrawerToggler onClick={toggleDrawer} rotateBtn={true} />}
+        >ClickUp Clone</Button>
+
+      </Drawer>
+    </>
+  );
+};
+
+export default NavDrawer;
