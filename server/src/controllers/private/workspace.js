6824b3354d9f649325cbaf05e9c2acd63852cfa6
Ashish Shevale <shevaleashish@gmail.com>
2022-01-13 22:03:30 +0530

    Create basic controllers for workspace related tasks

---
+++ server/src/controllers/private/workspace.js
@@ -0,0 +1,19 @@
+exports.getAllWorkspaces = async (req, res, next) => {
+  res.end("Get All Workspaces");
+}
+
+exports.createNewWorkspace = async (req, res, next) => {
+  res.end("Create New Workspace");
+}
+
+exports.getWorkspaceData = async (req, res, next) => {
+  res.end("Get Workspace Data");
+}
+
+exports.deleteWorkspace = async (req, res, next) => {
+  res.end("Delete Workspace");
+}
+
+exports.modifyWorkspace = async (req, res, next) => {
+  res.end("Modify Workspace");
+}
