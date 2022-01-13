bda4b95d01031e03a3b3c2723cb06d9538a3f736
Ashish Shevale <shevaleashish@gmail.com>
2022-01-13 22:56:02 +0530

    Create controller for creating a new workspace

--- server/src/controllers/private/workspace.js
+++ server/src/controllers/private/workspace.js
@@ -1,26 +1,66 @@
 const User = require("../../models/User");
+const Workspace = require("../../models/Workspace");
 const ErrorResponse = require("../../utils/errorResponse");
 
 exports.getAllWorkspaces = async (req, res, next) => {
   // Get which user made this request.
   // Here we are getting this value from the JWT
   // middleware as that is more secure.
-  const userId = req.user;
+  const user = req.user;
 
-  try {
-    const user = await User.findById(userId);
-    console.log(user.workspaces);
-    res.status(201).json({
-      success: true,
-      workspaces: user.workspaces,
-    })
-  } catch (error) {
-    return next(new ErrorResponse(error.message))
-  }
+  res.status(201).json({
+    success: true,
+    workspaces: user.workspaces,
+  });
 }
 
 exports.createNewWorkspace = async (req, res, next) => {
-  res.end("Create New Workspace");
+  const { workspaceName } = req.body;
+  const userId = req.user._id;
+  let workspace;
+  console.log("User id ", userId);
+
+  if (!workspaceName || workspaceName === "") {
+    return next(new ErrorResponse("Please provide name for workspace", 400));
+  }
+
+  // Create the workspace.
+  try {
+    workspace = new Workspace({
+      name: workspaceName,
+      userId: userId,
+    });
+  } catch (error) {
+    return next(new ErrorResponse("Failed to create workspace", 500));
+  }
+
+  // Associate this workspace with the correct user.
+  try {
+    // Find the user who made this request.
+    const user = await User.findById(userId);
+
+    // Add the new workspace's id to this user.
+    user.workspaces.push(workspace._id);
+    // Write to database.
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      workspace: workspace,
+    });
+  } catch (error) {
+    // In case the workspace was created but
+    // could not be associated with correct user,
+    // Delete the new workspace.
+    try {
+      await Workspace.findByIdAndDelete(workspace._id);
+    } catch (error) {
+      return next(new ErrorResponse("Workspace created, but unassociated. Failed to delete workspace", 500));
+    }
+
+    console.log(error.message);
+    return next(new ErrorResponse("Failed to associate new workspace with user. Deleted workspace", 500));
+  }
 }
 
 exports.getWorkspaceData = async (req, res, next) => {
