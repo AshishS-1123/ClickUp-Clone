e0bdacd852eae7a708d379590fee77f6a6b78409
Ashish Shevale <shevaleashish@gmail.com>
2022-01-13 22:02:49 +0530

    Create private route to handle all workspace related tasks

---
+++ server/src/routes/private/workspace.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const {
+  getAllWorkspaces,
+  createNewWorkspace,
+  getWorkspaceData,
+  deleteWorkspace,
+  modifyWorkspace
+} = require("../../controllers/private/workspace");
+
+const router = express.Router();
+
+// Routes for Workspace.
+// GET /workspace -> Get all workspaces of user. Just names
+// POST /workspace -> Create new workspace using name
+// DELETE /workspace/:id -> Delete workspace
+// GET /workspace/:id -> Get all data of workspace
+// PATCH /workspace/:id -> Change property of workspace.
+
+router.route("/")
+  .get(getAllWorkspaces)
+  .post(createNewWorkspace);
+
+router.route("/:id")
+  .get(getWorkspaceData)
+  .delete(deleteWorkspace)
+  .patch(modifyWorkspace);
+
+module.exports = router;
