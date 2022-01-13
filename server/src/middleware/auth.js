77756a8a4dfa1f9ed644fe39beeb8352e4f7ef35
Ashish Shevale <shevaleashish@gmail.com>
2022-01-13 22:03:11 +0530

    Middleware to verify JWT for authorised users

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -5,17 +5,22 @@ const ErrorResponse = require ('../utils/errorResponse')
 exports.protect = async (req, res, next) => {
   let token;
 
+  // Authenticated users will share the token in the Header as "Bearer {token}"
+  // We check if the token is present in the request.
   if (req.headers.authorization && req.headers.authorization.startsWith ("Bearer")) {
     token = req.headers.authorization.split (" ")[1]
   }
 
+  // If no token is present, the user is not authorised.
   if (!token) {
     return next (new ErrorResponse ("Not authorized to access this route", 401))
   }
 
   try {
+    // Decrypt the token.
     const decoded = jwt.verify (token, process.env.JWT_SECRET)
 
+    // Check if the token belongs to some user and is not some random string.
     const user = await User.findById (decoded.id)
 
     if (!user) {
