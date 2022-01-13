1ae6ecba79d1ba664f0dd4a3843d4537ee947aa1
Ashish Shevale <shevaleashish@gmail.com>
2022-01-13 22:05:26 +0530

    Remove unnecessary files

--- server/src/controllers/private.js
+++
@@ -1,6 +0,0 @@
-exports.getPrivateData = (req, res, next) => {
-  res.status (200).json ({
-    success: true,
-    data: "You got access to private data"
-  })
-}
--- server/src/routes/private.js
+++
@@ -1,9 +0,0 @@
-const express = require ('express')
-const router = express.Router ()
-
-const { getPrivateData } = require ('../controllers/private')
-const { protect } = require ('../middleware/auth')
-
-router.route ('/').get (protect, getPrivateData)
-
-module.exports = router
