33a9e38a21ba3e76929a91630e31ca29c74b6cf9
Ashish Shevale <shevaleashish@gmail.com>
2022-01-13 22:31:42 +0530

    Create controller to get all workspaces

--- server/src/controllers/private/workspace.js
+++ server/src/controllers/private/workspace.js
@@ -1,5 +1,22 @@
+const User = require("../../models/User");
+const ErrorResponse = require("../../utils/errorResponse");
+
 exports.getAllWorkspaces = async (req, res, next) => {
-  res.end("Get All Workspaces");
+  // Get which user made this request.
+  // Here we are getting this value from the JWT
+  // middleware as that is more secure.
+  const userId = req.user;
+
+  try {
+    const user = await User.findById(userId);
+    console.log(user.workspaces);
+    res.status(201).json({
+      success: true,
+      workspaces: user.workspaces,
+    })
+  } catch (error) {
+    return next(new ErrorResponse(error.message))
+  }
 }
 
 exports.createNewWorkspace = async (req, res, next) => {
