30c74c01cc62db6197ade6530c3526f804447169
Ashish Shevale <shevaleashish@gmail.com>
2022-01-15 19:03:03 +0530

    Bug fix in create request

--- client/utils/requests/workspaceRequests.js
+++ client/utils/requests/workspaceRequests.js
@@ -17,8 +17,10 @@ export const createNewWorkspace = async (userId, token, workspaceName) => {
 
   const headers = new Headers();
   headers.append("Authorization", `Bearer ${token}`);
+  headers.append("Content-Type", "application/json")
 
   const { data, status } = await makeRequest(url, "POST", body, headers);
+  return { data, status };
 }
 
 export const getWorkspaceData = () => {
