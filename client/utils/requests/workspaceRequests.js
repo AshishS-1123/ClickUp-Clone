af1e720e6ceda1bd18a18ec3dc8a9d837ce47d4b
Ashish Shevale <shevaleashish@gmail.com>
2022-01-15 14:05:43 +0530

    Create request to create workspaces

--- client/utils/requests/workspaceRequests.js
+++ client/utils/requests/workspaceRequests.js
@@ -11,8 +11,14 @@ export const getAllWorkspaces = async (userId, token) => {
   return { data, status };
 }
 
-export const createNewWorkspace = () => {
+export const createNewWorkspace = async (userId, token, workspaceName) => {
+  const url = `/user/${userId}/workspaces`;
+  const body = { workspaceName };
 
+  const headers = new Headers();
+  headers.append("Authorization", `Bearer ${token}`);
+
+  const { data, status } = await makeRequest(url, "POST", body, headers);
 }
 
 export const getWorkspaceData = () => {
