79fc9df987f2faac6425b7f54139ffce3c1d8d26
Ashish Shevale <shevaleashish@gmail.com>
2022-01-16 11:36:52 +0530

    Create basic controllers for space requests

---
+++ server/src/controllers/private/spaces.js
@@ -0,0 +1,20 @@
+exports.getAllSpaces = (req, res, next) => {
+  res.end("Get All Spaces");
+}
+
+exports.createNewSpace = (req, res, next) => {
+  res.end("Create New Space");
+}
+
+exports.getSpaceData = (req, res, next) => {
+  res.end("Get Space Data");
+}
+
+exports.deleteSpace = (req, res, next) => {
+  res.end("Delete Space");
+}
+
+exports.modifySpace = (req, res, next) => {
+  res.end("Modify Space");
+}
+
