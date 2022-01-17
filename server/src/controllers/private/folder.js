f11e4dab7df3f8559eb06193ea94d5df24175ed6
Ashish Shevale <shevaleashish@gmail.com>
2022-01-17 15:25:09 +0530

    Setup basic controller for folders

---
+++ server/src/controllers/private/folder.js
@@ -0,0 +1,20 @@
+
+exports.getAllFolders = async (req, res, next) => {
+  res.end("Get All Folders");
+}
+
+exports.getFolderData = async (req, res, next) => {
+  res.end("Get Folder Data");
+}
+
+exports.createNewFolder = async (req, res, next) => {
+  res.end("Create New Folder");
+}
+
+exports.deleteFolder = async (req, res, next) => {
+  res.end("Delete Folder");
+}
+
+exports.modifyFolder = async (req, res, next) => {
+  res.end("Modify Folder");
+}
