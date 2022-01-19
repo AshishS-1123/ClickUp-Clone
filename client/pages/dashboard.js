64fcbd2adcfb5000b859e9081625e85d2358ccce
Ashish Shevale <shevaleashish@gmail.com>
2022-01-19 14:36:51 +0530

    Dispatch action on UI

--- client/pages/dashboard.js
+++ client/pages/dashboard.js
@@ -3,18 +3,23 @@ import { useDispatch, useSelector } from "react-redux"
 import NavDrawer from "../features/NavBar";
 import protectedRoute from "../utils/ProtectedRoutes";
 import { getAllWorkspacesAsync } from "../redux/slices/workspaceSlice";
+import { getSpaceDataAsync } from "../redux/slices/spaceSlice";
 
 function Dashboard() {
   const dispatch = useDispatch();
   const userId = useSelector(state => state.authReducer.userId);
   const token = useSelector(state => state.authReducer.token);
+  // const activeSpaces = useSelector(state => state.workspaceReducer.activeWorkspaceChildren);
 
   useEffect(() => {
     // Dispatch request to fetch workspaces.
     dispatch(getAllWorkspacesAsync({ userId, token }))
-    // Dispatch request to fetch data for first workspace.
-    // The first workspace is the default one we show the user at startup.
-
+      .then((action) => {
+        const workspaceId = action.payload.workspaces[0].id;
+        action.payload.workspaces[0].spaces.forEach(spaceId => {
+          dispatch(getSpaceDataAsync({ spaceId, workspaceId, userId, token }));
+        });
+      })
   }, []);
 
   return (
