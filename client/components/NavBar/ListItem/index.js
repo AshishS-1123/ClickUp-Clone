92170de13da1f7599d63b35b25ec3bd168a08741
Ashish Shevale <shevaleashish@gmail.com>
2022-01-20 21:52:16 +0530

    Created ListItem component

---
+++ client/components/NavBar/ListItem/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "../SpaceItem/SpaceItem.module.css";
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
+
+function ListItem({ isActive, listName, nestingLevel = 10 }) {
+
+  let containerStyle = {
+    paddingLeft: `${nestingLevel}px`,
+  };
+
+  if (isActive) {
+    containerStyle = {
+      ...containerStyle,
+      background: "#3c3d39",
+      borderLeft: "3px solid #ffa12f"
+    }
+  }
+
+  return (
+    <div className={styles.spaceItem__container} style={containerStyle}>
+      <RadioButtonUncheckedIcon sx={{ width: "12px", height: "12px", color: "lightgrey" }} />
+      <div className={styles.spaceItem__title}>{listName}</div>
+    </div>
+  )
+}
+
+export default ListItem;
