baec0ac5d36e4fdb948838c2a03079a8c619bd75
Ashish Shevale <shevaleashish@gmail.com>
2022-01-22 20:54:31 +0530

    Show spaces in the container as per new revealer

--- client/features/NavBar/SpaceContainer/index.js
+++ client/features/NavBar/SpaceContainer/index.js
@@ -5,9 +5,9 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import SpaceItem from "../../../components/NavBar/SpaceItem";
-import ListItem from "../../../components/NavBar/ListItem";
+import Stack from "@mui/material/Stack";
 import "./SpaceContainer.module.css";
-import FolderItem from "../../../components/NavBar/FolderItem";
+// import { useSelector } from "react-redux";
 
 const spaceContainerStyles = {
   bgcolor: "#20262b",
@@ -18,7 +18,101 @@ const spaceContainerStyles = {
   }
 }
 
+const spaceData = {
+  "spaceData": [
+    {
+      "_id": "61e6e4cc54940295e4a57bfa",
+      "name": "Space 1",
+      "workspaceId": "61e6e4aa54940295e4a57bf0",
+      "userId": "61e6e48154940295e4a57bea",
+      "children": [
+        {
+          "childType": "FOLDER",
+          "id": "61e6e7bd433622bf9b41b039",
+          "_id": "61e6e7bd433622bf9b41b03c"
+        },
+        {
+          "childType": "FOLDER",
+          "id": "61e6e822418b1cd439d71583",
+          "_id": "61e6e822418b1cd439d71586"
+        }
+      ],
+      "spaceColor": "red",
+      "spaceAvatar": "icon",
+      "enabledViews": [
+        "LIST_VIEW"
+      ],
+      "activeStatuses": [
+        {
+          "_id": "61e6e4cc54940295e4a57bfb",
+          "label": "TODO",
+          "color": "#aeaeae"
+        }
+      ],
+      "doneStatuses": [],
+      "closedStatuese": [
+        {
+          "_id": "61e6e4cc54940295e4a57bfc",
+          "label": "DONE",
+          "color": "#00ee00"
+        }
+      ],
+      "__v": 2
+    }
+  ],
+  "folderData": [
+    {
+      "_id": "61e6e7bd433622bf9b41b039",
+      "name": "Folder 1 :: Space",
+      "userId": "61e6e48154940295e4a57bea",
+      "parent": {
+        "parentId": "61e6e4cc54940295e4a57bfa",
+        "parentType": "SPACE",
+        "_id": "61e6e7bd433622bf9b41b03a"
+      },
+      "children": [
+        {
+          "childType": "LIST",
+          "id": "61e6fc3e8dcaccd0ed80c820",
+          "_id": "61e6fc3f8dcaccd0ed80c823"
+        }
+      ],
+      "__v": 1
+    },
+    {
+      "_id": "61e6e822418b1cd439d71583",
+      "name": "Folder 1 :: Space",
+      "userId": "61e6e48154940295e4a57bea",
+      "parent": {
+        "parentId": "61e6e4cc54940295e4a57bfa",
+        "parentType": "SPACE",
+        "_id": "61e6e822418b1cd439d71584"
+      },
+      "children": [],
+      "__v": 0
+    }
+  ],
+  "listData": [
+    {
+      "_id": "61e6fc3e8dcaccd0ed80c820",
+      "name": "List Under Space 1",
+      "userId": "61e6e48154940295e4a57bea",
+      "parent": {
+        "parentId": "61e6e7bd433622bf9b41b039",
+        "parentType": "FOLDER",
+        "_id": "61e6fc3f8dcaccd0ed80c821"
+      },
+      "children": [],
+      "__v": 0
+    }
+  ]
+}
+
+
 function SpaceContainer() {
+  // const spaceData = useSelector(state => state.spaceReducer);
+  // console.log(spaceData);
+
   return (
     <>
       <Accordion sx={spaceContainerStyles} square disableGutters>
@@ -37,9 +131,13 @@ function SpaceContainer() {
         </AccordionSummary>
 
         <AccordionDetails sx={{ padding: "0px" }}>
-          <SpaceItem spaceName="Space 1" />
-          <ListItem listName="List 1" />
-          <FolderItem folderName="Folder 1" />
+          <Stack>
+            {
+              spaceData.spaceData.map(item => {
+                return <SpaceItem spaceName={item.name} contents={item.children} key={item._id} />
+              })
+            }
+          </Stack>
         </AccordionDetails>
       </Accordion>
     </>
