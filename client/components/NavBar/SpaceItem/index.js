bcac2f6a6083c5a84c9a69eb5f4daae57d6a0cfb
Ashish Shevale <shevaleashish@gmail.com>
2022-01-22 20:53:54 +0530

    Create space item with revealer for showing its children

--- client/components/NavBar/SpaceItem/index.js
+++ client/components/NavBar/SpaceItem/index.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./SpaceItem.module.css";
 import ShieldIcon from '@mui/icons-material/Shield';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import FolderItem from "../FolderItem";
+import ListItem from "../ListItem";
 
-function SpaceItem({ isActive, spaceName }) {
+function SpaceItem({ isActive, spaceName, contents }) {
+  const [revealerVisible, setRevealerVisible] = useState(false);
 
   let containerStyle = {};
 
@@ -14,11 +18,40 @@ function SpaceItem({ isActive, spaceName }) {
     }
   }
 
+  const showRevealer = (event) => {
+    setRevealerVisible(prev => !prev);
+  }
+
   return (
-    <div className={styles.spaceItem__container} style={containerStyle}>
-      <ShieldIcon sx={{ width: "20px", height: "20px" }} />
-      <div className={styles.spaceItem__title}>{spaceName}</div>
-    </div>
+    <>
+      <div className={styles.spaceItem__container} style={containerStyle}>
+
+        <div className={styles.spaceItem__titleContainer}>
+          <ShieldIcon sx={{ width: "20px", height: "20px" }} />
+          <div className={styles.spaceItem__title}>{spaceName}</div>
+        </div>
+
+        <KeyboardArrowDownIcon
+          onClick={showRevealer}
+          sx={{
+            transform: revealerVisible ? "rotate(180deg)" : "",
+          }}
+        />
+
+      </div>
+
+      <div style={{ display: revealerVisible ? "block" : "none" }}>
+        {
+          contents.map(item => {
+            if (item.childType == "FOLDER") {
+              return <FolderItem folderName={item.id} key={item.id} />
+            } else {
+              return <ListItem listName={item.id} key={item.id} />
+            }
+          })
+        }
+      </div>
+    </>
   )
 }
 
