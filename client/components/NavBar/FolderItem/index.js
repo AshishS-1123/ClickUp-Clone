553667c374209f1fab6d76d1b3efcdcb777cd490
Ashish Shevale <shevaleashish@gmail.com>
2022-01-22 22:06:17 +0530

    Create revealer for folder and other effects

--- client/components/NavBar/FolderItem/index.js
+++ client/components/NavBar/FolderItem/index.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../SpaceItem/SpaceItem.module.css";
 import FolderIcon from '@mui/icons-material/Folder';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import ListItem from "../ListItem";
 
-function FolderItem({ isActive, folderName, nestingLevel = 10 }) {
+function FolderItem({ isActive, folderName, contents, nestingLevel = 10 }) {
+  const [revealerVisible, setRevealerVisible] = useState(false);
 
   let containerStyle = {
     paddingLeft: `${nestingLevel}px`,
+    color: "white",
   };
 
   if (isActive) {
@@ -16,11 +20,41 @@ function FolderItem({ isActive, folderName, nestingLevel = 10 }) {
     }
   }
 
+  const showRevealer = (event) => {
+    setRevealerVisible(prev => !prev);
+  }
+
   return (
+    <>
     <div className={styles.spaceItem__container} style={containerStyle}>
-      <FolderIcon sx={{ width: "16px", height: "16px", color: "lightgrey" }} />
-      <div className={styles.spaceItem__title}>{folderName}</div>
-    </div>
+
+        <div className={styles.spaceItem__titleContainer}>
+          <FolderIcon sx={{ width: "16px", height: "16px", color: "lightgrey" }} />
+          <div className={styles.spaceItem__title}>{folderName}</div>
+        </div>
+
+        <KeyboardArrowDownIcon
+          onClick={showRevealer}
+          sx={{
+            transform: revealerVisible ? "rotate(180deg)" : "",
+            color: "white",
+          }}
+        />
+
+      </div>
+
+      <div style={{ display: revealerVisible ? "block" : "none" }}>
+        {
+          contents.map(item => {
+            if (item.itemType == "FOLDER") {
+              return <FolderItem folderName={item.name} key={item.id} nestingLevel={nestingLevel + 10} />
+            } else {
+              return <ListItem listName={item.name} key={item.id} nestingLevel={nestingLevel + 10} />
+            }
+          })
+        }
+      </div>
+    </>
   )
 }
 
