3b1638d0da2382c8511abbe1efb46f9e5fc662b7
Ashish Shevale <shevaleashish@gmail.com>
2022-02-01 13:22:32 +0530

    Create options menu for lists

--- client/components/NavBar/ListItem/index.js
+++ client/components/NavBar/ListItem/index.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import { setActive } from "../../../redux/slices/spaceSlice";
 import styles from "../SpaceItem/SpaceItem.module.css";
+import ListOptionsDialog from "./listOptionsDialog";
 
 function ListItem({ id, listName, nestingLevel = 10 }) {
   const isActive = useSelector(state => state.spaceReducer.activeItem) == id;
   const dispatch = useDispatch();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const openMenu = Boolean(anchorEl);
+  const [showIcons, setShowIcons] = useState(false);
+  const [revealerVisible, setRevealerVisible] = useState(false);
 
   let containerStyle = {
     paddingLeft: `${nestingLevel}px`,
@@ -21,16 +27,38 @@ function ListItem({ id, listName, nestingLevel = 10 }) {
     }
   }
 
+  const closeOptions = (event) => {
+    setAnchorEl(null);
+  }
+
+  const showOptions = (event) => {
+    setAnchorEl(event.currentTarget);
+  }
+
   const setCurrentAsActive = (event) => {
     dispatch(setActive({ id }));
   }
 
   return (
-    <div className={styles.item__container} style={containerStyle}>
+    <div c
+      lassName={styles.item__container}
+      style={containerStyle}
+      onMouseEnter={() => { setRevealerVisible(true) }}
+      onMouseLeave={() => { setRevealerVisible(false) }}
+    >
       <div className={styles.item__titleContainer} onClick={setCurrentAsActive}>
         <RadioButtonUncheckedIcon sx={{ width: "10px", height: "10px", color: "lightgrey" }} />
         <div className={styles.list__title}>{listName}</div>
+        <MoreHorizIcon
+          onClick={showOptions}
+          sx={{
+            visibility: (revealerVisible || openMenu) ? "visible" : "hidden",
+            transform: "scale(0.7)",
+            marginLeft: "15px",
+          }}
+        />
       </div>
+      <ListOptionsDialog anchorEl={anchorEl} openMenu={openMenu} closeMenu={closeOptions} />
     </div>
   )
 }
