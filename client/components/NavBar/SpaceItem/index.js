f45c191efff462a045db4cb8d8304e58bdb84965
Ashish Shevale <shevaleashish@gmail.com>
2022-02-01 12:22:27 +0530

    Options menu for spaces

--- client/components/NavBar/SpaceItem/index.js
+++ client/components/NavBar/SpaceItem/index.js
@@ -2,6 +2,12 @@ import React, { useState } from "react";
 import styles from "./SpaceItem.module.css";
 import ShieldIcon from '@mui/icons-material/Shield';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 import { useDispatch, useSelector } from "react-redux";
 import FolderItem from "../FolderItem";
 import ListItem from "../ListItem";
@@ -10,11 +16,21 @@ import { setActive } from "../../../redux/slices/spaceSlice";
 function SpaceItem({ id, spaceName, contents }) {
   const isActive = useSelector(state => state.spaceReducer.activeItem) == id;
   const [showIcons, setShowIcons] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const openMenu = Boolean(anchorEl);
   const dispatch = useDispatch();
 
   const [revealerVisible, setRevealerVisible] = useState(false);
 
-  let containerStyle = {};
+  let containerStyle = {
+    color: "white",
+  };
+
+  const menuItemStyle = {
+    padding: "8px",
+    color: "#d5d6d7",
+    fontSize: "13px",
+  }
 
   if (isActive) {
     containerStyle = {
@@ -26,6 +42,14 @@ function SpaceItem({ id, spaceName, contents }) {
 
   const showRevealer = (event) => {
     setRevealerVisible(prev => !prev);
+  }
+
+  const showOptions = (event) => {
+    setAnchorEl(event.currentTarget);
+  }
+
+  const closeOptions = (event) => {
+    setAnchorEl(null);
   }
 
   const setCurrentAsActive = (event) => {
@@ -42,17 +66,27 @@ function SpaceItem({ id, spaceName, contents }) {
       >
 
         <div className={styles.item__titleContainer} onClick={setCurrentAsActive}>
-          <ShieldIcon sx={{ width: "20px", height: "20px" }} />
+          <ShieldIcon sx={{ width: "20px", height: "20px", color: "#ffa12f" }} />
           <div className={styles.item__title}>{spaceName}</div>
         </div>
 
-        <KeyboardArrowDownIcon
-          onClick={showRevealer}
-          sx={{
-            transform: revealerVisible ? "rotate(180deg)" : "",
-            display: showIcons ? "block" : "none",
-          }}
-        />
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <MoreHorizIcon
+            onClick={showOptions}
+            sx={{
+              visibility: showIcons ? "visible" : "hidden",
+              transform: "scale(0.7)"
+            }}
+          />
+          <KeyboardArrowDownIcon
+            onClick={showRevealer}
+            sx={{
+              transform: revealerVisible ? "rotate(180deg) scale(0.7)" : "scale(0.7)",
+              display: showIcons ? "block" : "none",
+            }}
+          />
+        </div>
+
 
       </div>
 
@@ -67,6 +101,28 @@ function SpaceItem({ id, spaceName, contents }) {
           })
         }
       </div>
+
+      <Menu
+        sx={{
+          "& .MuiMenu-paper": {
+            background: "#384047",
+            color: "#979797",
+          },
+          "& li": {
+            fontSize: "13px",
+          }
+        }}
+        anchorEl={anchorEl}
+        open={openMenu}
+        onClose={closeOptions}
+      >
+        <h5 style={{ fontWeight: "600", padding: "5px 20px", fontSize: "11px" }}>SPACE SETTINGS</h5>
+        <MenuItem style={menuItemStyle}><AddIcon />Add Folder</MenuItem>
+        <MenuItem style={menuItemStyle}><AddIcon />Add List</MenuItem>
+        <MenuItem style={menuItemStyle}><EditIcon />Edit</MenuItem>
+        <MenuItem style={menuItemStyle}><DeleteIcon />Delete</MenuItem>
+
+      </Menu>
     </>
   )
 }
---
+++ client/components/NavBar/SpaceItem/spaceOptionsDialog.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function SpaceOptionsDialog() {
+  return (
+    <></>
+  )
+}
+
+export default SpaceOptionsDialog;
