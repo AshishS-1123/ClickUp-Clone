96651ef0622303e1cd5e5034d1ccba03e3e49e02
Ashish Shevale <shevaleashish@gmail.com>
2022-02-23 19:36:36 +0530

    Create basic model for task

--- server/src/models/Task.js
+++ server/src/models/Task.js
@@ -1,9 +1,22 @@
-name
-  in => which list / folder / space it belongs to
-description
-asignee
-priority(urgent, high, normal, low)
-start data
-due date
-subtasks => unordered list of items to do
+const mongoose = require("mongoose");
 
+const TaskSchema = mongoose.Schema({
+  // Name of this Task.
+  name: {
+    type: String,
+    required: [true, "Task must have a name"],
+  },
+  userId: {
+    type: String,
+    required: [true, "Provide User when creating tasks"],
+  },
+  // Parent of this task and whether its a folder or space.
+  parent: {
+    type: { parentId: String, parentType: String },
+    required: [true, "Tasks must have parents"],
+  },
+})
+
+const Task = mongoose.model("Task", TaskSchema);
+
+module.exports = Task;
