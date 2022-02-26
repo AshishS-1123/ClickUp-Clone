d85dd62d556df5c63cd2fbf9fc4c1823ebcc4425
Ashish Shevale <shevaleashish@gmail.com>
2022-02-26 19:36:10 +0530

    Basic setup for tests

---
+++ client/components/Dialogs/__test__/createFolderDialog.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react'
+import CreateFolderDialog from "../createListDialog";
+
+describe('Home', () => {
+  it('renders a heading', () => {
+    expect(true).toEqual(true);
+  })
+})
