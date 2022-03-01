4e4665d79f866e91a32b8150b082687ae17debc8
Ashish Shevale <shevaleashish@gmail.com>
2022-03-01 23:45:32 +0530

    Fix lint for dialogs

---
+++ client/components/Dialogs/CreateFolderDialog/FolderDialog.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Dialog from '@mui/material/Dialog';
+import CloseIcon from '@mui/icons-material/Close';
+import styles from '../dialog.module.css';
+
+function CreateFolderDialog({ open, closeDialog, handleCreateFolder }, ref) {
+  return (
+    <Dialog
+      open={open}
+      onBackdropClick={closeDialog}
+      sx={{
+        '& .MuiDialog-paper': {
+          width: '550px',
+          height: '419px',
+          position: 'absolute',
+          top: '10px',
+          background: '#2b343b',
+        },
+      }}
+    >
+      <div className={styles.title_bar}>
+        <h1 className={styles.title}>Create new folder</h1>
+        <CloseIcon
+          sx={{
+            position: 'absolute',
+            top: '25px',
+            right: '25px',
+            color: 'white',
+          }}
+          onClick={closeDialog}
+        />
+      </div>
+      <div className={styles.content}>
+        <label htmlFor="folderDialog_name" id="dialog_nameLabel">Folder name</label>
+        <input type="text" id="folderDialog_name" placeholder="Enter folder name" ref={ref} />
+      </div>
+      <button
+        className={styles.dialog_button}
+        onClick={handleCreateFolder}
+      >
+        Create folder
+      </button>
+    </Dialog>
+  );
+}
+
+export default React.forwardRef(CreateFolderDialog);
--- client/components/Dialogs/CreateFolderDialog/__test__/FolderDialog.test.jsx
+++ client/components/Dialogs/CreateFolderDialog/__test__/FolderDialog.test.jsx
@@ -1,44 +1,47 @@
-import React from "react";
-import { getByPlaceholderText, render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import FolderDialog from "../FolderDialog";
+import React from 'react';
+import { getByPlaceholderText, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import FolderDialog from '../FolderDialog';
 
 let component = null;
 
-describe("<FolderDialog /> tests", () => {
-  it("should match snapshot", () => {
+describe('<FolderDialog /> tests', () => {
+  it('should match snapshot', () => {
     component = render(
       <FolderDialog
-        open={true}
+        open
         closeDialog={() => { }}
         handleCreateFolder={() => { }}
         ref={null}
-      />);
+      />,
+    );
 
     expect(component.container).toMatchSnapshot();
-  })
+  });
 
-  it("should render without crashing", () => {
+  it('should render without crashing', () => {
     component = render(
       <FolderDialog
-        open={true}
+        open
         closeDialog={() => { }}
         handleCreateFolder={() => { }}
         ref={null}
-      />);
-  })
+      />,
+    );
+  });
 
   it("should be displayed when 'open' prop is passed", () => {
     component = render(
       <FolderDialog
-        open={true}
+        open
         closeDialog={() => { }}
         handleCreateFolder={() => { }}
         ref={null}
-      />);
+      />,
+    );
 
-    expect(screen.getByRole("dialog")).toBeDefined();
-  })
+    expect(screen.getByRole('dialog')).toBeDefined();
+  });
 
   it("should not be displayed when 'open' prop is passed", () => {
     component = render(
@@ -47,74 +50,76 @@ describe("<FolderDialog /> tests", () => {
         closeDialog={() => { }}
         handleCreateFolder={() => { }}
         ref={null}
-      />);
+      />,
+    );
 
-    expect(screen.queryByRole("dialog")).toBeNull();
-  })
+    expect(screen.queryByRole('dialog')).toBeNull();
+  });
 
-  it("should close when backdrop is clicked", () => {
+  it('should close when backdrop is clicked', () => {
     const closeDialog = jest.fn();
     const handleCreateFolder = jest.fn();
 
     component = render(
       <FolderDialog
-        open={true}
+        open
         closeDialog={closeDialog}
         handleCreateFolder={handleCreateFolder}
         ref={null}
-      />);
+      />,
+    );
 
     // Get the backdrop.
-    const backdrop = screen.getByRole("dialog").parentElement;
+    const backdrop = screen.getByRole('dialog').parentElement;
 
     // Click on the backdrop.
     userEvent.click(backdrop);
 
     // Assert that the closeDialog fundtion was called.
     expect(closeDialog).toHaveBeenCalledTimes(1);
+  });
 
-  })
-
-  it("should should close when close icon is clicked", () => {
+  it('should should close when close icon is clicked', () => {
     const closeDialog = jest.fn();
     const handleCreateFolder = jest.fn();
 
     component = render(
       <FolderDialog
-        open={true}
+        open
         closeDialog={closeDialog}
         handleCreateFolder={handleCreateFolder}
         ref={null}
-      />);
+      />,
+    );
 
     // Get the close icon button.
-    const button = screen.getByTestId("CloseIcon");
+    const button = screen.getByTestId('CloseIcon');
 
     // Click on the button.
     userEvent.click(button);
 
     // Assert that the closeDialog fundtion was called.
     expect(closeDialog).toHaveBeenCalledTimes(1);
-  })
+  });
 
-  it("should have a title, close icon, input element and submit button", () => {
+  it('should have a title, close icon, input element and submit button', () => {
     component = render(
       <FolderDialog
-        open={true}
+        open
         closeDialog={jest.fn()}
         handleCreateFolder={jest.fn()}
         ref={null}
-      />);
+      />,
+    );
 
     // Check label is present.
-    expect(screen.getByLabelText("Folder name")).toBeDefined();
+    expect(screen.getByLabelText('Folder name')).toBeDefined();
     // Check input is present.
-    expect(screen.getByPlaceholderText("Enter folder name")).toBeDefined();
+    expect(screen.getByPlaceholderText('Enter folder name')).toBeDefined();
     // Check submit button is present
-    expect(screen.getByText("Create folder")).toBeDefined();
-  })
-})
-
+    expect(screen.getByText('Create folder')).toBeDefined();
+  });
+});
 
 /*
 
