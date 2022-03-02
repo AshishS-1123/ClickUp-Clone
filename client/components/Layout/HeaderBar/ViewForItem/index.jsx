09b6b42470e74d163fd2c2dc087bbc2f59c446c3
Ashish Shevale <shevaleashish@gmail.com>
2022-03-02 21:19:44 +0530

    Create static ViewForItem component to represent active item

---
+++ client/components/Layout/HeaderBar/ViewForItem/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import ShieldIcon from '@mui/icons-material/Shield';
+
+const containerStyles = {
+  width: '100px',
+  height: '50px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-around',
+  marginLeft: '10px',
+};
+
+const textStyles = {
+  fontSize: '16px',
+  color: '#d5d6d7',
+  fontWeight: '500',
+}
+
+function ViewForItem() {
+  const { activeItemName } = useSelector(state => state.spaceReducer.activeItemName);
+
+  return (
+    <div style={containerStyles}>
+      <ShieldIcon sx={{ width: '32px', height: '32px', color: '#ffa12f' }} />
+      <p style={textStyles}>{activeItemName || "Space 1"}</p>
+    </div>
+  )
+}
+
+export default ViewForItem;
