0a8e0895b3a8eb66040aa2d9d3e56bd9a74fc1d4
Ashish Shevale <shevaleashish@gmail.com>
2022-03-12 20:34:20 +0530

    Algorithm to group tasks by custom key. Use for views

---
+++ client/utils/taskAlgorithms/groupAlgorithm.js
@@ -0,0 +1,27 @@
+export const groupableValues = {
+  PRIORITY: 'priority',
+  STATUS: 'status',
+}
+
+function getInitialValueFromKeys(possibleKeys) {
+  const initialValue = {};
+
+  possibleKeys.forEach(key => {
+    initialValue[key._id] = { tasks: [] };
+  })
+
+  return initialValue;
+}
+
+function groupTasks(tasks, groupBy, possibleKeys) {
+  const initialValue = getInitialValueFromKeys(possibleKeys);
+
+  tasks.forEach(task => {
+    const thisKey = task[groupBy]._id;
+    initialValue[thisKey].tasks.push(task);
+  })
+
+  return initialValue;
+}
+
+export default groupTasks;
