85b77ec8feeaf23397703f9f0bb4d2871b7d41de
Ashish Shevale <shevaleashish@gmail.com>
2022-03-15 20:43:20 +0530

    Create custom scrollable component

---
+++ client/features/Views/GridView/Kanban/components/ScrollableLane.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ScrollableLane({ children }) {
+  return (
+    <div style={{ width: '257px' }}>
+      {children}
+    </div>
+  )
+}
+
+export default ScrollableLane;
