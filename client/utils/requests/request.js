2756bc5665e6c5b8661007779370a7f3af2d5ec9
Ashish Shevale <shevaleashish@gmail.com>
2022-07-05 18:41:19 +0530

    Fix url for backend requests

--- client/utils/requests/request.js
+++ client/utils/requests/request.js
@@ -1,5 +1,5 @@
-const URL = 'http://localhost:5000/api/v1';
-// const URL = 'https://fathomless-coast-92467.herokuapp.com/api/v1';
+// const URL = 'http://localhost:5000/api/v1';
+const URL = 'https://fathomless-coast-92467.herokuapp.com/api/v1';
 
 async function makeRequest(urlPath, method, body, reqHeaders) {
   // Create the whole url.
