7e35651045a7e85ec08b5c1a92d8853b045236cb
Ashish Shevale <shevaleashish@gmail.com>
2022-07-05 18:23:26 +0530

    Pre-deploy

--- client/next.config.js
+++ client/next.config.js
@@ -1,15 +1,7 @@
-// const { withGoogleFonts } = require('nextjs-google-fonts');
-
-// module.exports = withGoogleFonts({
-//   googleFonts: {
-//     fonts: [
-//       'https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap',
-//     ],
-//   },
-// });
 
 module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
+  basePath: '/ClickUp-Clone',
 }
