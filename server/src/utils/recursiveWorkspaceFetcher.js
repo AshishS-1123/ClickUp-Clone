9f62a66032ff7a7c76b62642af44ab7ecab21b0f
Ashish Shevale <shevaleashish@gmail.com>
2022-07-08 18:41:53 +0530

    Refactor get workspace route to return all data recursively.

---
+++ server/src/utils/recursiveWorkspaceFetcher.js
@@ -0,0 +1,76 @@
+// Returns the data of all children of the given workspace.
+// The format of return value is 
+// {spaces: [...arrray of all spaces], lists: [...array of all lists]}
+
+const { validateSpace, validateFolder, validateList, validateTask } = require("../middleware/paramValidator");
+
+// on the front end, we can then reconstruct this to form the tree.
+exports.fetchWorkspaceRecur = async (spaceIds, userId) => {
+    const spaces = [];
+    const lists = [];
+    const folders = [];
+    const tasks = [];
+
+    let queue = spaceIds.map(spaceId => {
+        return {childType: "SPACE", id: spaceId};
+    });
+
+    // repeat until all items in the queue have been processed.
+    while (queue.length !== 0) {
+        // get the topmost item.
+        const currentItem = queue.shift();
+
+        // process the current item.
+        switch (currentItem.childType) {
+            case "SPACE":
+                // get space data.
+                const space = await validateSpace(currentItem.id, userId);
+
+                // add this space to the global space store.
+                spaces.push({
+                    id: space._id,
+                    name: space.name,
+                });
+
+                // add children to the queue.
+                queue = [...queue, ...space.children];
+                break;
+            case "FOLDER":
+                // get folder data.
+                const folder = await validateFolder(currentItem.id, userId);
+
+                // add this folder to the global folder store.
+                folders.push ({
+                    id: folder._id,
+                    name: folder.name
+                });
+
+                // add children to queue.
+                queue = [...queue, ...folder.children];
+                break;
+            case "LIST":
+                // get list data.
+                const list = await validateList(currentItem.id, userId);
+
+                // add this list to the global list store.
+                lists.push ({
+                    id: list._id,
+                    name: list.name
+                });
+
+                // add children to queue.
+                queue = [...queue, ...list.children];
+                break;
+            case "TASK":
+                // get task data.
+                const task = await validateTask(currentItem.id, userId);
+
+                // add this task to the global task store.
+                tasks.push (task);
+                break;
+        }
+    }
+
+    // return the computed value.
+    return {spaces, folders, lists, tasks};
+}
