4c408e1701b8f0bd99b6980b416fc44566ab4357
Ashish Shevale <64722310+AshishS-1123@users.noreply.github.com>
2023-08-21 10:44:55 +0000

    Init NextJS and Install Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,21 @@
+/** @type { import('@storybook/nextjs').StorybookConfig } */
+const config = {
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
